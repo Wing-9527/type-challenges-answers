0de7d41cffa815051edd34dd3d046b5cf35e4de0
Ian Ting <2698300641@qq.com>
2022-11-16 22:29:12 +0800

    feat: save 00539 answer

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,1 +1,14 @@
-type Merge<F, S> = any
+type Merge<F, S, Type = Omit<F, keyof S> & S> = {
+  [K in keyof Type]: Type[K]
+}
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
+
+type S = Merge<Foo, Bar>
